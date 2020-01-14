6b9718dd5ad6eb4fbc2a2a70e7d74cf7f9c2467c
Lars Niesen <iah71niesen@gso-koeln.de>
2020-01-14 10:21:35 +0100

    changed user

--- config.def.h
+++ config.def.h
@@ -1,6 +1,6 @@
 /* user and group to drop privileges to */
-static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *user  = "lars";
+static const char *group = "users";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
